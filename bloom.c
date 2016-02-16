f0e4e5331035fbd54e0d44b012b3ae4c0f0c90d0
awacs <haroldzwang@gmail.com>
2016-02-16 18:48:33 -0500

    2/16

--- bloom.c
+++ bloom.c
@@ -1,6 +1,49 @@
 #include <stdint.h>
 #include <stdio.h>
-uint64_t hash1(uint64_t key, uint64_t len)
+#include "bloom.h"
+#include <inttypes.h>
+void bloom_init(bloom_filter_t *B, index_t size_in_bits)
+{
+    B->table = (uint64_t *) calloc(size_in_bits/sizeof(index_t)+1,sizeof(index_t));
+	index_t key =0; 
+    if (B->table == NULL) {
+		perror("malloc failed");
+		free(B->table);
+    }
+	B->count = 0;
+    B->size = size_in_bits;
+}
+void bloom_destroy(bloom_filter_t *B){
+	free(B->table);
+}
+
+index_t get_bit(bloom_filter_t *B, index_t i)
+{
+	if (i>(B->size-1)||i<0){
+		perror("Error: ");
+		return 99;
+	}
+	index_t key;
+	key = 1;
+	key = key<<B->size-1-i;
+	index_t array;
+	array = *B->table & key;
+	array = array>>B->size-1-i; 
+	return array;
+}
+void set_bit(bloom_filter_t *B, index_t i)
+{
+	index_t key;
+	key = 1;
+	key = key<<B->size-1-i;
+	*B->table = *B->table | key;
+	/* printf("array=%" PRIu64 "\n", array);
+	return array; */
+	
+}
+
+
+index_t hash1(bloom_filter_t *B, ourkey_t key)
 {
   key = ~key + (key << 15); // key = (key << 15) - key - 1;
   key = key ^ (key >> 12);
@@ -8,10 +51,10 @@ uint64_t hash1(uint64_t key, uint64_t len)
   key = key ^ (key >> 4);
   key = key * 2057; // key = (key + (key << 3)) + (key << 11);
   key = key ^ (key >> 16);
-  return key % len;
+  return key % B->size;
 }
 
-uint64_t hash2(uint64_t key, uint64_t len)
+index_t hash2(bloom_filter_t *B, ourkey_t key)
 {
    printf("argument=%d\n", key);
    key = (key+0x7ed55d16) + (key<<12);
@@ -20,11 +63,7 @@ uint64_t hash2(uint64_t key, uint64_t len)
    key = (key+0xd3a2646c) ^ (key<<9);
    key = (key+0xfd7046c5) + (key<<3);
    key = (key^0xb55a4f09) ^ (key>>16);
-   printf("argument=%d\n", key);
-   key = key % len;
-   printf("argument=%d\n", key);
-   printf("len=%d\n", len);
-   return key;
+   return key % B->size;
 }
 
 uint64_t main(uint64_t argc, char **argv)
@@ -36,12 +75,26 @@ uint64_t main(uint64_t argc, char **argv)
     printf("argument needed.\n");
     return -1;
   }
-	uint64_t len=100;
+	bloom_filter_t Bloom;
+	index_t size_in_bits = 3;
+	index_t index1 = 1;
+
+	bloom_init(&Bloom, size_in_bits);
+	index_t first_bit;
+	first_bit = get_bit(&Bloom,index1); 
+	printf("first_bit=%" PRIu64 "\n", first_bit);
+	set_bit(&Bloom,index1);
+	first_bit = get_bit(&Bloom,index1); 
+	printf("first_bit=%" PRIu64 "\n", first_bit);
+	//printf("count=%d\n", Bloom->count);
+	/* uint64_t len=100;
 	uint64_t hasha;
 	uint64_t hashb;
 	hasha=hash1(argument,len);
 	hashb=hash2(argument,len);
 	printf("hash1=%d\n", hasha);
-	printf("hash2=%d\n", hashb);
-	
+	printf("hash2=%d\n", hashb); */
+
+	bloom_destroy(&Bloom);
+
 }
--- bloom.h
+++ bloom.h
@@ -3,7 +3,7 @@
 #include <stdint.h>
 
 typedef uint64_t index_t;
-typedef uint64_t key_t;
+typedef uint64_t ourkey_t;
 typedef struct {
   index_t size; // in bits
   index_t count; // in bits
@@ -11,13 +11,13 @@ typedef struct {
 } bloom_filter_t;
 #define N_HASHES 3
 
-void set_bit(bloom_filter_t *B, index_t i);
-index_t get_bit(bloom_filter_t *B, index_t i);
+void set_bit(bloom_filter_t *B, index_t i);//done
+index_t get_bit(bloom_filter_t *B, index_t i);//done
 
-index_t hash1(bloom_filter_t *B, key_t k);
-index_t hash2(bloom_filter_t *B, key_t k);
+index_t hash1(bloom_filter_t *B, ourkey_t k);
+index_t hash2(bloom_filter_t *B, ourkey_t k);
 
-void bloom_init(bloom_filter_t *B, index_t size_in_bits);
-void bloom_destroy(bloom_filter_t *B);
-int bloom_check(bloom_filter_t *B, key_t k);
-void bloom_add(bloom_filter_t *B, key_t k);
+void bloom_init(bloom_filter_t *B, index_t size_in_bits);//done
+void bloom_destroy(bloom_filter_t *B);//done
+int bloom_check(bloom_filter_t *B, ourkey_t k);
+void bloom_add(bloom_filter_t *B, ourkey_t k);
